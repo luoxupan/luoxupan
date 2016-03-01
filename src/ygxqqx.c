e479f9d06aede5537a026583aca6dca5ec21ff90
ygxqqx <ygxqqx@163.com>
2016-03-01 21:35:36 +0800

    添加epoll

--- src/ygxqqx.c
+++ src/ygxqqx.c
@@ -1,30 +1,62 @@
 #include "util.h"
+#include "http.h"
+#include "epoll.h"
 
-int main(){
+extern struct epoll_event *events;
+
+int main(int argc, char* argv[]){
 
 	int sockfd;
+	int rc;
 	socklen_t acceptlen;
 	struct sockaddr_in clientaddr;
 	if((sockfd = startServer(3000)) == -1)
 		return -1;
+	rc = makeSocketNoBlocking(sockfd);
+	check(rc == 0, "makeSocketNoBlocking");
+
+	int epfd = yg_epoll_create(0);
+	struct epoll_event event;
+	event.data.fd = sockfd;
+	event.events = EPOLLIN | EPOLLET;
+	yg_epoll_add(epfd, sockfd, &event);
 
 
 	while(1){
-		logInfo("read to accept");
-		int acceptfd = accept(sockfd, (struct sockaddr*)&clientaddr, &acceptlen);
-		if(acceptfd == -1){
-			if((errno == EAGAIN) || (errno ==EWOULDBLOCK)){
+		logInfo("read to wait");
+		int n;
+		n = yg_epoll_wait(epfd, events, MAXEVENTS, -1);
 
+		int i;
+		for(i=0; i<n; i++){
+			if((events[i].events & EPOLLERR) ||
+                (events[i].events & EPOLLHUP) ||
+                (!(events[i].events & EPOLLIN))){
+				logErr("epoll error");
+				close(events[i].data.fd);
 				continue;
-			}else {
-				logErr("accept");
-				break;
+			}
+			else if(sockfd == events[i].data.fd){
+				while(1){
+					logInfo("read to accept");
+					int acceptfd = accept(sockfd, (struct sockaddr*)&clientaddr, &acceptlen);
+					if(acceptfd == -1){
+						if((errno == EAGAIN) || (errno ==EWOULDBLOCK)){
+
+							continue;
+						}else {
+							logErr("accept");
+							break;
+						}
+					}
+
+					doRequest(acceptfd);
+					close(acceptfd);
+				}
 			}
 		}
-
-		doRequest(acceptfd);
-		close(acceptfd);
 	}
+
 
 	return 0;
 }
